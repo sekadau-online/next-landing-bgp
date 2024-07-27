48f6921eb72013a6cb02196ee358b772a6fd34dd
sekadau-online <74548531+sekadau-online@users.noreply.github.com>
2024-07-27 09:36:03 +0700

    next-landing-bgp

---
+++ components/Layout/Footer.js
@@ -0,0 +1,88 @@
+import React from "react";
+import LogoVPN from "../../public/assets/Logo.svg";
+import Facebook from "../../public/assets/Icon/facebook.svg";
+import Twitter from "../../public/assets/Icon/twitter.svg";
+import Instagram from "../../public/assets/Icon/instagram.svg";
+import Image from 'next/image';
+import LogoBGP from '../../public/assets/Logo.png';
+const Footer = () => {
+  return (
+    <div className="bg-white-300 pt-44 pb-24">
+      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
+        <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
+          {/* <LogoVPN className="h-8 w-auto mb-6" /> */}
+          <Image 
+      src={LogoBGP}
+      alt="BGPWESTKAL.ID"
+      width={130}
+      height={30}
+      // blurDataURL="data:..." automatically provided
+      // placeholder="blur" // Optional blur-up while loading
+    />
+          <p className="mb-4">
+          <p className="mb-4"></p>
+          
+            <strong className="font-medium">Borneo global Persada</strong> Mengajak Anda untuk melihat lebih jauh, di mana teknologi dan komitmen kami bertemu untuk menciptakan masa depan yang terhubung dan berkelanjutan bagi semua.
+          </p>
+          <div className="flex w-full mt-2 mb-8 -mx-2">
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Facebook className="h-6 w-6" />
+            </div>
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Twitter className="h-6 w-6" />
+            </div>
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Instagram className="h-6 w-6" />
+            </div>
+          </div>
+          <p className="text-gray-400">©{new Date().getFullYear()} - BGPWESTKAL.ID</p>
+        </div>
+        {/* <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Head Office</p>
+          <ul className="text-black-500 ">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+            Jalan Penanjung tumenggung III
+RT/RW 012/003 Desa Mungguk
+Kec. Sekadau Hilir, Kab. Sekadau
+Kalimantan Barat 
+            </li>
+          </ul>
+        </div> */}
+        <div className="row-span-2 sm:col-span-2 sm:col-start-9 sm:col-end-11 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Head Office</p>
+          <ul className="text-black-500">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+            Jalan Penanjung tumenggung III
+RT/RW 012/003 Desa Mungguk
+Kec. Sekadau Hilir, Kab. Sekadau
+Kalimantan Barat  {" "}
+            </li>
+
+          </ul>
+        </div>
+        <div className="row-span-2 sm:col-span-2 sm:col-start-11 sm:col-end-13 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Contact Us</p>
+          <ul className="text-black-500 ">
+          <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+            08236788883 {" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+            contact@bgpwestkal.id{" "}
+            </li>
+          <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+             <a href="https://marimoli.com">
+              Blog{" "}
+              </a> 
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+            <a href="https://wa.me/082367888883" >Become Partner{" "}</a>
+              
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Testimoni.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+
+// import react slick
+import Slider from "react-slick";
+import Image from "next/image";
+import Stars from "../public/assets/Icon/stars.svg";
+import ArrowBack from "../public/assets/Icon/eva_arrow-back-fill.svg";
+import ArrowNext from "../public/assets/Icon/eva_arrow-next-fill.svg";
+
+const Testimoni = ({
+  listTestimoni = [
+    {
+      name: "Sarah Putri",
+      image: "/assets/people-2.png",
+      city: "Sekadau",
+      country: "Indonesia",
+      rating: "4.5",
+      testimoni:
+        "Saya bekerja dari rumah dan stabilitas internet adalah hal yang sangat penting bagi saya. Borneo Global Persada memberikan koneksi internet yang cepat dan andal, yang membuat pekerjaan saya menjadi jauh lebih mudah. Saya sangat puas dengan layanan mereka!",
+    },
+    {
+      name: "Ahmad Rizal",
+      image: "/assets/people-1.png",
+      city: "Sekadau",
+      country: "Indonesia",
+      rating: "4.5",
+      testimoni:
+        "Sebagai pemilik toko online, kecepatan internet adalah kunci untuk mengelola transaksi dan mengupdate stok produk secara real-time. Layanan internet dari Borneo Global Persada telah membantu bisnis saya berjalan dengan lancar tanpa gangguan. Terima kasih atas dukungannya!",
+    },
+    {
+      name: "Rudi Setiawan",
+      image: "/assets/people-3.png",
+      city: "Sekadau",
+      country: "Indonesia",
+      rating: "4.5",
+      testimoni:
+        "Saya mengembangkan aplikasi web dan sering melakukan upload dan download file besar. Koneksi internet dari Borneo Global Persada sangat membantu dalam meningkatkan produktivitas saya. Latensi rendah dan kecepatan tinggi adalah keuntungan besar bagi pekerjaan saya.",
+    },
+  ],
+}) => {
+  const settings = {
+    dots: true,
+    customPaging: function (i) {
+      return (
+        <a className="">
+          <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
+        </a>
+      );
+    },
+    dotsClass: "slick-dots w-max absolute mt-20  ",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const [sliderRef, setSliderRef] = useState(null);
+
+  return (
+    <>
+      <Slider
+        {...settings}
+        arrows={false}
+        ref={setSliderRef}
+        className="flex items-stretch justify-items-stretch"
+      >
+        {listTestimoni.map((listTestimonis, index) => (
+          <div className="px-3 flex items-stretch" key={index}>
+            <div className="border-2 border-gray-500 hover:border-orange-500 transition-all rounded-lg p-8 flex flex-col">
+              <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
+                <div className="flex order-2 xl:order-1">
+                  <Image
+                    src={listTestimonis.image}
+                    height={50}
+                    width={50}
+                    alt="Icon People"
+                  />
+                  <div className="flex flex-col ml-5 text-left">
+                    <p className="text-lg text-black-600 capitalize">
+                      {listTestimonis.name}
+                    </p>
+                    <p className="text-sm text-black-500 capitalize">
+                      {listTestimonis.city},{listTestimonis.country}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
+                  <p className="text-sm">{listTestimonis.rating}</p>
+                  <span className="flex ml-4">
+                    <Stars className="h-4 w-4" />
+                  </span>
+                </div>
+              </div>
+              <p className="mt-5 text-left">“{listTestimonis.testimoni}”.</p>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      <div className="flex w-full items-center justify-end">
+        <div className="flex flex-none justify-between w-auto mt-14">
+          <div
+            className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+            onClick={sliderRef?.slickPrev}
+          >
+            <ArrowBack className="h-6 w-6 " />
+          </div>
+          <div
+            className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+            onClick={sliderRef?.slickNext}
+          >
+            <ArrowNext className="h-6 w-6" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Testimoni;
